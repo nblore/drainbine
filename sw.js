70f4972800ce504f5f90b014cb33178aad0a5c0b
nblore <nblore@deloitte.co.uk>
2019-03-08 12:31:35 +0000

    Added about page

--- sw.js
+++ sw.js
@@ -1,19 +1,17 @@
-importScripts('/cache-polyfill.js');
+const PRECACHE='precache-v1';
+const RUNTIME = 'runtime';
 
-self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('drainbine').then(function(cache) {
-     return cache.addAll([
-       '/',
-       '/index.html',
-       '/styles/master.css',
-       '/styles/bootstrap.css',
-       '/styles/bootstrap.min.css',
-       '/javascript/drain-map.js'
-     ]);
-   })
- );
-});
+const PRECACHE_URLS = [
+  '/',
+  'index.html',
+  'index.html>homescreen=1',
+  '?homescreen=1',
+  'styles/master.css',
+  'styles/bootstrap.css',
+  'styles/bootstrap.min.css',
+  'javascript/drain-map.js',
+  'manifest.json',
+]
 
 self.addEventListener('fetch', function(event) {
     console.log(event.request.url);
